36af8702b7238b41cd777247062facc0f6df2021
William Elí O.L <ramun346416@gmail.com>
2022-11-20 16:41:13 -0600

    Update 337_La_abuela_Maria_acepta_el_reto.c

--- 337_La_abuela_Maria_acepta_el_reto.c
+++ 337_La_abuela_Maria_acepta_el_reto.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 void main()
 {
-int diente, diente1, i, j, k, acum, acum1, diente_Superio[6], diente_Inferior[6];
+int diente, diente1, i, j, k, acum, acum1, diente_Superio[6], diente_Inferior[6], ident,  ident1;
 //float azul 
 
 // printf("\nDame el numero del primer tipo de diente \n");
 // scanf("%d", &diente);
 // printf("\nDame el numero del segundo tipo de diente \n");
 // scanf("%d", &diente1);
+ident=0;
 j=0;
 k=0;
 diente=0;
@@ -37,9 +38,24 @@ diente1=0;
         printf("\nNumero de dientes fuera de rango, coloca 2 numeros numeros inferiores o iguales a 2,000 \n");
         else 
         {
-            diente_Superio[j]=diente;
+            diente_Inferior[j]=diente1;
             j+1;
         }
         }
+        ident=0;
+    for (i=0;i<6;i++)
+        
+        {
+                if ((diente_Inferior[i]) == (diente_Superio[i]) )
+                ident++;
+        }
+    if (ident<=1)
+
+    printf("\n %d SI \n", ident);
+        
+    if (ident>1)
+        printf("\n%d NO \n", ident);
+        
+
     // }
 }
